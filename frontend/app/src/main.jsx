a953feb417b860eee68c616b597439b26b6e1191
MoazAlsabouh <ibrahaem005@gmail.com>
2025-08-05 21:16:08 +0300

    add FrontEnd files

--- frontend/app/src/main.jsx
+++ frontend/app/src/main.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// 1. تأكد من استيراد BrowserRouter
 import { BrowserRouter } from 'react-router-dom'
+import { NotificationProvider } from './context/NotificationContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
+    {/* 2. استخدام BrowserRouter مع تحديد المسار الأساسي */}
+    <BrowserRouter basename="/restaurant-management/">
+      <NotificationProvider>
+        <App />
+      </NotificationProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
