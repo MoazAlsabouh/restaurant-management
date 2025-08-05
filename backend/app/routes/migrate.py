a431517c7032e98f29f947ad93ef19471f0f4646
MoazAlsabouh <ibrahaem005@gmail.com>
2025-08-05 20:18:30 +0300

    Delete migrate endpoint

--- backend/app/routes/migrate.py
+++ backend/app/routes/migrate.py
@@ -1,9 +1,10 @@
-# from flask_migrate import upgrade
-# from flask import Blueprint
+from flask_migrate import upgrade
+from flask import Blueprint
 
-# migrate_bp = Blueprint('migrate_bp', __name__)
+migrate_bp = Blueprint('migrate_bp', __name__)
 
-# @migrate_bp.route('/')
-# def run_migrations():
-#     upgrade()
-#     return "Migrations applied!"
+@migrate_bp.route('/')
+def run_migrations():
+    # upgrade()
+    # return "Migrations applied!"
+    return "this endpoint is for running migrations, but it's currently disabled."
